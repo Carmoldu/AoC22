3d5996adc6f1b8e1d12e3819787ee9bae96eafb7
cmolins <carles.molins.duran@outlook.com>
2022-12-06 00:16:09 +0100

    Day5

---
+++ Day5/problem1.py
@@ -0,0 +1,51 @@
+import re
+
+def process_initial_state(file):
+    stacks = {n:[] for n in range(1,10)}
+    with open(file) as f:
+        for line in f.readlines():
+            for stack in stacks:
+                stack_content = line[(stack - 1) * 3 + stack]
+
+                if stack_content == ' ':
+                    continue
+
+                stacks[stack].insert(0, stack_content)
+
+    return stacks
+
+
+def process_planned_moves(file):
+    with open(file) as f:
+        moves = [tuple(map(int, re.findall(r'\d+', line))) for line in f.readlines()]
+
+    return moves
+
+
+def remove_blocks(stack, n_blocks):
+    return stack[:-n_blocks], stack[-n_blocks:][::-1]
+
+
+def add_blocks(stack, blocks):
+    return stack + blocks
+
+
+def execute_moves(stacks, moves):
+    for n_blocks, from_stack, to_stack in moves:
+        stacks[from_stack], blocks_being_moved = remove_blocks(stacks[from_stack], n_blocks)
+        stacks[to_stack] = add_blocks(stacks[to_stack], blocks_being_moved)
+
+    return stacks
+        
+
+def main():
+    stacks = process_initial_state('Day5/input_stacks.txt')
+    moves = process_planned_moves('Day5/input_moves.txt')
+
+    stacks = execute_moves(stacks, moves)
+
+    print([stack[-1] for stack in stacks.values()])
+
+
+if __name__ == '__main__':
+    main()
